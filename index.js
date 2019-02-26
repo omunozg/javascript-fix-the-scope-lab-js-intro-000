506e2a9b6d71b6f7209a2c90aaf556aba7885210
Oscar Munoz <omunozg@emory.edu>
2019-02-26 19:06:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var theFunk = function funkyFunction()
-  return theFunk {
+var theFunk = function funkyFunction(){
+  return theFunk
     return "FUNKY!"
 }
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
